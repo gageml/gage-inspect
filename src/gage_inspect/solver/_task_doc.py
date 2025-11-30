1433da359a2e86afdf8af57d1eac2a59b8c251a9
Garrett Smith <garrett@placeon.earth>
2025-11-29 18:11:58 -0600

    Cleanup task doc solver w/tests

--- src/gage_inspect/solver/_task_doc.py
+++ src/gage_inspect/solver/_task_doc.py
@@ -1,5 +1,5 @@
 import yaml
-
+from inspect_ai import Task
 from inspect_ai.solver import Generate, Solver, TaskState, solver
 
 from gage_inspect.patch import patch_active_task, patch_task_description
@@ -41,84 +41,94 @@ INPUT_TEMPLATE = '<{name} description="{description}">{value}</{name}>'
 @solver
 def task_doc(
     prologue: str | None = None,
-    output_description: str | None = None,
     inputs: str | None = None,
+    output_description: str | None = None,
 ) -> Solver:
     async def solve(state: TaskState, generate: Generate) -> TaskState:
-        # Solver only works with input strings
-        if isinstance(state.input, list):
-            raise ValueError(
-                f"task_doc solver requires string input but got {state.input!r}"
-            )
-
-        # Get docstring for active task
-        task = active_task()
-        doc = parse_task_doc(task.attribs.get("__doc__") or "")
-        if not doc.description:
-            raise ValueError("task_doc solver requires a docstring with a description")
-
-        # Prompt starts with the task description
-        prompt = [
-            resolve(
-                prologue or PROLOGUE_TEMPLATE, task_description=doc.description.strip()
-            )
-        ]
-
-        # Task inputs
-        if doc.params:
-            # If one param, use input as param value
-            if len(doc.params) == 1:
-                param = doc.params[0]
-                prompt.append(
-                    resolve(
-                        inputs or INPUTS_TEMPLATE,
-                        INPUT_TEMPLATE.format(
-                            name=param.arg_name,
-                            description=param.description or "",
-                            value=state.input_text,
-                        ),
-                    )
-                )
-            # Multiple params require parseable YAML/JSON for values
-            else:
-                try:
-                    data = yaml.safe_load(state.input_text)
-                except Exception as e:
-                    raise ValueError(f"input must be valid YAML/JSON: {e}")
-                else:
-                    prompt.append(
-                        resolve(
-                            inputs or INPUTS_TEMPLATE,
-                            "\n".join(
-                                [
-                                    INPUT_TEMPLATE.format(
-                                        name=param.arg_name,
-                                        description=param.description or "",
-                                        value=data.get(param.arg_name)
-                                        or "not specified",
-                                    )
-                                    for param in doc.params
-                                ]
-                            ),
-                        )
-                    )
-        else:
-            prompt.append(INPUTS_TEMPLATE.format(inputs=state.input_text))
-
-        # Expected output
-        if doc.returns:
-            prompt.append(
-                resolve(
-                    output_description or OUTPUT_DESCRIPTION_TEMPLATE,
-                    doc.returns.description,
-                )
-            )
-
-        # Set user prompt and generate response
-        state.user_prompt.content = "\n\n".join(prompt)
+        state.user_prompt.content = task_doc_prompt(
+            active_task(),
+            state.input_text,
+            prologue,
+            inputs,
+            output_description,
+        )
         return await generate(state)
 
     return solve
+
+
+def task_doc_prompt(
+    task: Task,
+    input: str,
+    prologue: str | None = None,
+    inputs: str | None = None,
+    output_description: str | None = None,
+):
+    # Get docstring for active task
+    doc = parse_task_doc(task.attribs.get("__doc__") or "")
+    if not doc.description:
+        raise ValueError("task_doc solver requires a docstring with a description")
+
+    # Prompt starts with the task description
+    prompt = [
+        resolve(
+            prologue or PROLOGUE_TEMPLATE,
+            task_description=doc.description.strip(),
+        )
+    ]
+
+    # Task inputs
+    if doc.params:
+        # If one param, use input as param value
+        if len(doc.params) == 1:
+            param = doc.params[0]
+            prompt.append(
+                resolve(
+                    inputs or INPUTS_TEMPLATE,
+                    INPUT_TEMPLATE.format(
+                        name=param.arg_name,
+                        description=param.description or "",
+                        value=input,
+                    ),
+                )
+            )
+        # Multiple params require parseable YAML/JSON for values
+        else:
+            try:
+                data = yaml.safe_load(input)
+            except Exception as e:
+                raise ValueError(f"input must be valid YAML/JSON: {e}")
+            else:
+                if not isinstance(data, dict):
+                    raise ValueError("input must be a dict/map of named values")
+                prompt.append(
+                    resolve(
+                        inputs or INPUTS_TEMPLATE,
+                        "\n".join(
+                            [
+                                INPUT_TEMPLATE.format(
+                                    name=param.arg_name,
+                                    description=param.description or "",
+                                    value=data.get(param.arg_name) or "not specified",
+                                )
+                                for param in doc.params
+                            ]
+                        ),
+                    )
+                )
+    else:
+        prompt.append(resolve(INPUTS_TEMPLATE, input))
+
+    # Expected output
+    if output_description or doc.returns:
+        prompt.append(
+            resolve(
+                output_description or OUTPUT_DESCRIPTION_TEMPLATE,
+                doc.returns.description if doc.returns else "",
+            )
+        )
+
+    return "\n\n".join(prompt)
 
 
 def resolve(template: str, *args, **kw) -> str:
---
+++ tests/solver/test_task_doc_solver.py
@@ -0,0 +1,218 @@
+from inspect_ai import Task, task
+from pytest import raises
+
+from gage_inspect.solver._task_doc import task_doc, task_doc_prompt
+from gage_inspect.task import run_task
+
+
+@task
+def description_only():
+    """Reverse the letters in a word."""
+    return Task()
+
+
+def test_description_only():
+    assert (
+        task_doc_prompt(description_only(), "abc")
+        == """
+Perform this task:
+
+<task_description>
+Reverse the letters in a word.
+</task_description>
+
+Here are the inputs:
+
+<inputs>
+abc
+</inputs>
+""".strip()
+    )
+
+
+@task
+def runnable():
+    """Do a thing."""
+    return Task(solver=task_doc())
+
+
+def test_runnable():
+    # Run the task
+    resp = run_task(runnable(), "abc", model="mockllm/model")
+    assert resp.output.error is None
+    assert resp.sample.messages[0].content == (
+        """
+Perform this task:
+
+<task_description>
+Do a thing.
+</task_description>
+
+Here are the inputs:
+
+<inputs>
+abc
+</inputs>
+""".strip()
+    )
+
+
+@task
+def input():
+    """Reverse the letters in a word.
+
+    Input:
+      value: the word to reverse
+    """
+    return Task()
+
+
+def test_input():
+    assert (
+        task_doc_prompt(input(), "abc")
+        == """
+Perform this task:
+
+<task_description>
+Reverse the letters in a word.
+</task_description>
+
+Here are the inputs:
+
+<inputs>
+<value description="the word to reverse">abc</value>
+</inputs>
+""".strip()
+    )
+
+
+@task
+def input_and_output():
+    """Reverse the letters in a word.
+
+    Input:
+      value: the word to reverse
+
+    Output:
+      Reversed word
+    """
+    return Task()
+
+
+def test_input_and_output():
+    assert (
+        task_doc_prompt(input_and_output(), "abc")
+        == """
+Perform this task:
+
+<task_description>
+Reverse the letters in a word.
+</task_description>
+
+Here are the inputs:
+
+<inputs>
+<value description="the word to reverse">abc</value>
+</inputs>
+
+The output should conform to this description:
+
+<output_description>
+Reversed word
+</output_description>
+
+Do not explain the results. Do not include inputs or other context. Do
+not apply formatting. Return only the generated output value.
+""".strip()
+    )
+
+
+@task
+def no_description():
+    return Task()
+
+
+def test_no_description():
+    with raises(ValueError) as e:
+        task_doc_prompt(no_description(), "abc")
+
+    assert e.value.args[0] == "task_doc solver requires a docstring with a description"
+
+
+@task
+def multiple_inputs():
+    """Subtract.
+
+    Input:
+      lhs: Left hand side of subtract operation
+      rhs: Right hand side of subtract operation
+    """
+    return Task()
+
+
+def test_multiple_inputs():
+    assert (
+        task_doc_prompt(multiple_inputs(), "{lhs: two mice, rhs: one mouse}")
+        == """
+Perform this task:
+
+<task_description>
+Subtract.
+</task_description>
+
+Here are the inputs:
+
+<inputs>
+<lhs description="Left hand side of subtract operation">two mice</lhs>
+<rhs description="Right hand side of subtract operation">one mouse</rhs>
+</inputs>
+""".strip()
+    )
+
+    # Inputs require value YAML/JSON input
+    with raises(ValueError) as e:
+        task_doc_prompt(multiple_inputs(), "[invalid inpu")
+
+    assert e.value.args[0].startswith("input must be valid YAML/JSON")
+
+    # Multiple inputs require a map/dict
+    with raises(ValueError) as e:
+        task_doc_prompt(multiple_inputs(), "123")
+
+    assert e.value.args[0].startswith("input must be a dict/map of named values")
+
+
+def test_custom_template():
+    assert (
+        task_doc_prompt(
+            description_only(),
+            "abc",
+            prologue="Do a thing.",
+            inputs="Use this: {}",
+            output_description="Return that.",
+        )
+        == """
+Do a thing.
+
+Here are the inputs:
+
+<inputs>
+abc
+</inputs>
+
+Return that.
+""".strip()
+    )
+
+    assert (
+        task_doc_prompt(description_only(), "abc", prologue="{x} and {y}")
+        == """
+{x} and {y}
+
+Here are the inputs:
+
+<inputs>
+abc
+</inputs>
+""".strip()
+    )
